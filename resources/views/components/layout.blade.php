febadd5de8b9e4de49bee95b52f5f7e93a103994
victor braz <victor.90braz@gmail.com>
2023-10-27 15:43:51 +0200

    <p>{{ session('success') }}</p>

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -66,4 +66,10 @@
             </div>
         </footer>
     </section>
+
+    @if (session()->has('success'))
+        <div class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">
+            <p>{{ session('success') }}</p>
+        </div>
+    @endIf
 </body>
