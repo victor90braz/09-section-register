bb2888d856a95666458bfe53aa2d7a07e645d70c
victor braz <victor.90braz@gmail.com>
2023-10-27 16:16:00 +0200

    {{ auth()->user()->name }}

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,7 +28,12 @@
             </div>
 
             <div class="mt-8 md:mt-0">
-                <a href="/register" class="text-xs font-bold uppercase">Register</a>
+
+                @auth
+                    <span class="text-xs font-bold uppercase"> Welcome, {{ auth()->user()->name }}</span>
+                @else
+                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                @endauth
 
                 <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
