2f8412dc2f1d39770f6a4831f0147fd7fcc2a393
victor braz <victor.90braz@gmail.com>
2023-10-27 16:08:53 +0200

    public const HOME = '/'; middleware('guest');

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::get('register', [RegisterController::class, 'create']);
-Route::post('register', [RegisterController::class, 'store']);
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
