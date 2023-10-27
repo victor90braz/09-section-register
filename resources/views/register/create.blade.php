0957de6e062e373493af9f0eef5fe25e443e2f3f
victor braz <victor.90braz@gmail.com>
2023-10-27 11:28:59 +0200

    create a html form

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -4,11 +4,27 @@
             <h1 class="text-center font-bold text-xl">Register</h1>
 
             <form action="/register" method="POST" class="mt-10">
+
+                <div class="mb-6">
+                    <label  class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                            for="name"
+                    >
+                        name
+                    </label>
+
+                    <input  class="border border-gray-400 p-2 w-full"
+                            type="text"
+                            name="name"
+                            id="name"
+                            required
+                    />
+                </div>
+
                 <div class="mb-6">
                     <label  class="block mb-2 uppercase font-bold text-xs text-gray-700"
                             for="username"
                     >
-                        Username
+                        username
                     </label>
 
                     <input  class="border border-gray-400 p-2 w-full"
@@ -17,6 +33,29 @@
                             id="username"
                             required
                     />
+                </div>
+
+                <div class="mb-6">
+                    <label  class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                            for="password"
+                    >
+                        password
+                    </label>
+
+                    <input  class="border border-gray-400 p-2 w-full"
+                            type="password"
+                            name="password"
+                            id="password"
+                            required
+                    />
+                </div>
+
+                <div class="mb-6">
+                    <button class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
+                            type="submit"
+                    >
+                        Submit
+                    </button>
                 </div>
             </form>
         </main>
