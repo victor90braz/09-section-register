5f0dc1290a37d57ba2aba5b971de7e218fb8fec8
victor braz <victor.90braz@gmail.com>
2023-10-27 15:54:17 +0200

    ->with('success', 'Your account has been created.');

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -42,8 +42,8 @@ class RegisterController extends Controller
 
         User::create($attributes);
 
-        session()->flash('success', 'Your account has been created.');
+        // session()->flash('success', 'Your account has been created.');
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
