a938a2908278d8331a766cd79e193ec3223f12ea
victor braz <victor.90braz@gmail.com>
2023-10-28 05:00:36 +0200

    validate the request

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\Rules\Exists;
+
 class SessionController extends Controller
 {
 
     public function create() {
 
         return view("sessions.create");
+    }
+
+    public function store() {
+
+        // validate the request
+
+        request()->validate([
+            'email' => ['required', 'exists:users,email'],
+            'password' => ['required']
+        ])
+
+        // attempt to authenticate and log in the user
+        // based on the provided credentials
+
+        // redirect with a success flash message
     }
 
     public function destroy() {
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
 Route::get('login', [SessionController ::class, 'create'])->middleware('guest');
+Route::post('login', [SessionController ::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionController ::class, 'destroy'])->middleware('auth');
 
