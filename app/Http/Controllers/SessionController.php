3464bbe5c3358a397780afe1c7f63a47f293a5ed
victor braz <victor.90braz@gmail.com>
2023-10-28 04:49:43 +0200

    create a login page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 class SessionController extends Controller
 {
+
+    public function create() {
+
+        return view("sessions.create");
+    }
+
     public function destroy() {
 
         auth()->logout();
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::post('logout', [SessionController ::class, 'destroy']);
+Route::get('login', [SessionController ::class, 'create'])->middleware('guest');
+
+Route::post('logout', [SessionController ::class, 'destroy'])->middleware('auth');
+
